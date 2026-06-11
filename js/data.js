9f3de85cc21a00204fc4564df5cc590c741c298b
torealm-creator <tore_alm@icloud.com>
2026-06-12 00:35:27 +0200

    Oppdatering 12.6.2026, 00:35:27

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
-// Oppdatert: 12.6.2026, 00:33:06
+// Oppdatert: 12.6.2026, 00:35:27
 const TRIP_DATA = {
 
   glympse: {
-    active: false,
+    active: true,
     url:    "https://www.google.com/maps/@60.2247997,11.0275539,1848m/data=!3m2!1e3!4b1!4m5!7m4!1m2!1s106739946593338097911!2sChZ6eHJ0YVNJa1k0bG12TnVmMjNMSTRBEggHBlQB5IU_dQ%3D%3D!2e2?hl=no&entry=ttu&g_ep=EgoyMDI2MDYwOS4wIKXMDSoASAFQAw%3D%3D",
   },
 
