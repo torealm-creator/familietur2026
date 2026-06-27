18f2e299c7f4537daed0f9b94f233c7ac903ce3e
torealm-creator <tore_alm@icloud.com>
2026-06-27 07:05:44 +0200

    Oppdatering 27.6.2026, 07:05:44

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-// Oppdatert: 27.6.2026, 06:44:01
+// Oppdatert: 27.6.2026, 07:05:44
 const TRIP_DATA = {
 
   contact: {
@@ -38,6 +38,12 @@ const TRIP_DATA = {
 
   posts: [
     {
+      id: "post_1782536744065", date: "27. juni 2026", location: "Larvik",
+      title: "Dag 1 - Larvik til Hirtshals", excerpt: "Første etappe i boks! Vi har rullet fra Nannestad ned til Larvik – 185,5 km på 2 timer og 9 minutter kjøretid. Nå står v...",
+      fullText: "Første etappe i boks! Vi har rullet fra Nannestad ned til Larvik – 185,5 km på 2 timer og 9 minutter kjøretid. Nå står vi i kø for å kjøre ombord på SuperSpeed 2. Fire timer over Skagerrak, og så er vi i Hirtshals.\n\nZeekr-en har taklet de første kilometerne uten problemer – god plass til fire personer og bagasje. Forbruket har ligget på 44,2 kWh over 185,5 km, som blir omtrent 24 kWh/100 km. Litt over WLTP-tallene, men helt forventet med motorvei og fullpakket bil. Lovende start!\n\nNeste stopp: Hirtshals, og videre ned gjennom Danmark mot Tyskland. Ringe venter i kveld!", image: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782536740/meprmgfipqemzsvt3mjt.png",
+      extraImages: [], featured: false,
+    },
+    {
       id: "post_1782073603715", date: "21. juni 2026", location: "Nannestad, Norge",
       title: "Mot Altea, tredje gang, ny bil, samme leilighet!", excerpt: "Kun kort tid til avreise og siste finpuss på det tekniske. Ladekablene er pakket, ruta er planlagt (en ny en, som alltid...",
       fullText: "Kun kort tid til avreise og siste finpuss på det tekniske. Ladekablene er pakket, ruta er planlagt (en ny en, som alltid – vi har aldri kjørt samme vei to ganger), og bilen er klar for sin første store prøve sørover.\n\nFor i år bytter vi hest i løpet: det er ikke Marias Xpeng G9 som skal kjøre oss til Spania, men min nye Zeekr 7X. Spennende å se hvordan den takler turen.\n\nTore, Maria, Christoffer og Charlotte ruller sørover. Caroline blir hjemme og jobber – og dermed mister vi vår innebygde shoppingradar. Ingen som peker ut \"den butikken der ser spennende ut\" når vi kjører gjennom en by denne gangen.\n\nReisemålet er som vanlig: leiligheten som mamma og Pål eier i Altea. Vi vet hvor nøklene henger, hvor de beste tomatene er, og at balkongen er perfekt for en kald drikke etter en lang kjøretur.\n\nNy bil, ny rute, samme deilige reisemål. Caroline – vi tar bilder!\n\nGod sommer! ☀️", image: "bilder/0C041B0E-8176-44FA-8688-B738D83F110D.png",
@@ -46,7 +52,6 @@ const TRIP_DATA = {
   ],
 
   photos: [
-    { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782535426/xplt3cvhywom6vtdfgmo.jpg", caption: "Ble full bil" },
     { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782535302/utkyx7tx56buvqiyf1xc.jpg", caption: "Klar for colorline" }
   ],
 
