7f8a9f5da308f2d725e7b4e3b9992fe1aa558f1a
torealm-creator <tore_alm@icloud.com>
2026-06-29 09:57:01 +0200

    Oppdatering 29.6.2026, 09:57:01

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-// Oppdatert: 29.6.2026, 08:34:41
+// Oppdatert: 29.6.2026, 09:57:00
 const TRIP_DATA = {
 
   contact: {
@@ -70,6 +70,7 @@ const TRIP_DATA = {
   ],
 
   photos: [
+    { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782719795/ct6j2iqqk9dp3zc5hg4v.png", caption: "Hotel Mondesir" },
     { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782587464/kqpceuvhhcvagfeke2q8.jpg", caption: "Middag i meppen" },
     { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782554035/dv7uwhpfopdzbnkirpws.jpg", caption: "Sjøluft" },
     { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782553970/fnpnewpoyrsnhq2zmyot.jpg", caption: "Rett ved Hirtshals" },
