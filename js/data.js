f08cb4cb822e6cbe4b456083e3861b19f6bf4ec7
torealm-creator <tore_alm@icloud.com>
2026-06-30 09:49:15 +0200

    Oppdatering 30.6.2026, 09:49:15

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-// Oppdatert: 30.6.2026, 09:48:37
+// Oppdatert: 30.6.2026, 09:49:14
 const TRIP_DATA = {
 
   contact: {
@@ -88,6 +88,7 @@ const TRIP_DATA = {
   ],
 
   photos: [
+    { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782805725/omn1ibsovlnbpmr0h86b.jpg", caption: "" },
     { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782768170/b2t8kumsasiccdkerbd3.jpg", caption: "Sansenastian" },
     { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782767914/ujipkgth65ds9rwdgurg.jpg", caption: "Tapaskongen" },
     { src: "https://res.cloudinary.com/daakblz3f/image/upload/f_auto,q_auto,w_1600/v1782737518/qj02mfspnbkgpochkwef.jpg", caption: "Kongen" },
